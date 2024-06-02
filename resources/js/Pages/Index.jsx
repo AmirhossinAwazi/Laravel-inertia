808a220b531a49699013ad36d043c556637d2497
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-02 10:25:06 +0330

    add the pages for CRUD

---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,24 @@
+import { Link } from '@inertiajs/inertia-react';
+import { router } from '@inertiajs/react'
+
+export default function Index({ posts }) {
+    function deletePost(id) {
+        router.delete(`/posts/${id}`);
+    }
+
+    return (
+        <>
+            <h1>My Super Blog</h1>
+            <Link href="/posts/create">create</Link>
+            <hr />
+            {posts && posts.map((post) => (
+                <div key={post.id}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                    <button type="button" onClick={() => deletePost(post.id)}>Delete</button>
+                    <Link href={`/posts/${post.id}/edit`}>edit</Link>
+                </div>
+            ))}
+        </>
+    )
+}
