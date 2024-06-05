8eb3e2f53ab4d0272166f2b35837be0f780fbc36
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-05 11:08:09 +0330

    ref (PostController) : delete the Explanations

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,9 +8,6 @@ use Inertia\Inertia;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return Inertia::render('Index', [
@@ -18,17 +15,11 @@ class PostController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return Inertia::render('Create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         Post::create([
@@ -39,9 +30,6 @@ class PostController extends Controller
         return to_route('posts.index');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Post $post)
     {
         return Inertia::render('Show', [
@@ -49,9 +37,6 @@ class PostController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Post $post)
     {
         return Inertia::render('Edit', [
@@ -59,9 +44,6 @@ class PostController extends Controller
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Post $post)
     {
         $post->update([
@@ -72,9 +54,6 @@ class PostController extends Controller
         return to_route('posts.index');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Post $post)
     {
         $post->delete();
