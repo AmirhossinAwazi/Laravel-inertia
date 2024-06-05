e7790ee1cec3d52162e9647ecaeada9224865478
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-05 11:07:07 +0330

    ref (web) : add the '/' to original page create

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,16 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
 
-Route::resource('/posts', PostController::class);
+Route::resource('/', PostController::class);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
