387545f68a619fc898f1b5195c037efec3f480d9
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-06 12:42:06 +0330

    feat (web) : add the route '/' and return to Index

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
+Route::get('/', function(){
+    return Inertia::render('Index');
+});
 Route::resource('/posts', PostController::class);
