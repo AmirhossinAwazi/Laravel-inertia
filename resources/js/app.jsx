d7c1f1de0d329f1a2e8202d3ae44cfaa515060ad
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-06 18:46:58 +0330

    feat (Package) : add the progress package, import that and some custom

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {createRoot} from 'react-dom/client'
 import {createInertiaApp } from '@inertiajs/inertia-react'
 import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers'
+import { InertiaProgress } from '@inertiajs/progress'
 
 createInertiaApp({
     // Below you can see that we are going to get all React components from resources/js/Pages folder
@@ -9,4 +10,20 @@ createInertiaApp({
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />)
     },
-})
+});
+
+InertiaProgress.init(
+    {
+        // The delay after which the progress bar will appear, in milliseconds...
+        delay: 250,
+    
+        // The color of the progress bar...
+        color: 'red',
+    
+        // Whether to include the default NProgress styles...
+        includeCSS: true,
+    
+        // Whether the NProgress spinner will be shown...
+        showSpinner: true,
+      },
+    )
