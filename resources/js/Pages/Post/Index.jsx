ea4f5989ec5d73513269e1f2ffafdf43095cc37a
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-08 19:35:07 +0330

    feat (index) : the change the layout and some css

--- resources/js/Pages/Post/Index.jsx
+++ resources/js/Pages/Post/Index.jsx
@@ -1,3 +1,4 @@
+import Layout from '@/Components/Shared/Layout';
 import Nav from '@/Components/Shared/Nav';
 import { Link } from '@inertiajs/inertia-react';
 import { router } from '@inertiajs/react'
@@ -9,25 +10,28 @@ export default function Index({ posts, time }) {
 
     return (
         <>
-            <h1 class="mt-6 font-bold text-4xl">Home</h1>
-            <Nav/>
+            <Layout>
 
-            <hr />
-            {posts && posts.map((post) => (
-                <div key={post.id}>
-                    <h2>{post.title}</h2>
-                    <p>{post.body}</p>
-                    <button type="button" onClick={() => deletePost(post.id)}>Delete</button>
-                    <Link href={`/posts/${post.id}/edit`}>edit</Link>
+
+                {/* <Nav/> */}
+
+                <hr />
+                {posts && posts.map((post) => (
+                    <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                        <button type="button" onClick={() => deletePost(post.id)}>Delete</button>
+                        <Link href={`/posts/${post.id}/edit`}>edit</Link>
+                    </div>
+                ))}
+
+                <div className="mt-64">
+                    <p>{time}</p>
+                    <Link href="/" class="text-blue-500" preserve-Scroll>
+                        Refresh
+                    </Link>
                 </div>
-            ))}
-
-            <div className="mt-64">
-                <p>{time}</p>
-                <Link href="/" class="text-blue-500" preserve-Scroll>
-                    Refresh
-                </Link>
-            </div>
+            </Layout>
         </>
     )
 }
