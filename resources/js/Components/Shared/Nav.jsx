9b9901dda27c46aeacf53aa8a30550fb7c52bbe7
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-08 18:20:42 +0330

    refactor (components) : nav components

--- resources/js/Components/Shared/Nav.jsx
+++ resources/js/Components/Shared/Nav.jsx
@@ -1,19 +1,19 @@
 import { Link } from "@inertiajs/inertia-react";
 
-export default function Nav() {
+export default function Nav(){
     return (
         <nav className="mt-6">
-            <ul class="list-disc">
-                <li>
-                    <Link href="/posts/create" as="button" class="text-blue-500 hover:underline">Create</Link>
-                </li>
-                <li>
-                    <Link href="/posts" as="button" class="text-blue-500 hover:underline">Posts</Link>
-                </li>
-                <li>
-                    <Link href="/logout" method="post" data={{ foo: 'bar' }} as="button" class="text-blue-500 hover:underline">Logout</Link>
-                </li>
-            </ul>
-        </nav>
+        <ul class="list-disc">
+        <li>
+            <Link href="/posts/create" as="button" class="text-blue-500 hover:underline">Create</Link>
+        </li>
+        <li>
+            <Link href="/posts" as="button" class="text-blue-500 hover:underline">Posts</Link>
+        </li>
+        <li>
+            <Link href="/logout" method="post" data={{ foo: 'bar' }} as="button" class="text-blue-500 hover:underline">Logout</Link>
+        </li>
+    </ul>
+    </nav>
     );
 }
