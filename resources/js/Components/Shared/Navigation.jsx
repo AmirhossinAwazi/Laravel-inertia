b7616c0389901141358a71337f420f8d5ce1ac1c
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-09 10:45:34 +0330

    feat (Navigation) : add the some method for links the change then we use them

--- resources/js/Components/Shared/Navigation.jsx
+++ resources/js/Components/Shared/Navigation.jsx
@@ -1,25 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, usePage } from '@inertiajs/inertia-react';
 import classNames from 'classnames';
 
 const Navigation = () => {
-  const { url } = usePage().props;
+  const [activeLink, setActiveLink] = useState(
+    localStorage.getItem('activeLink') || usePage().props.url
+  );
+
+  const handleClick = (path) => {
+    setActiveLink(path);
+    localStorage.setItem('activeLink', path);
+  };
 
   const getClassNames = (path) => classNames('text-blue-500 hover:underline', {
-    'font-bold underline': url === path,
+    'font-bold underline': activeLink === path,
   });
+
+  useEffect(() => {
+    const handlePopState = () => {
+      setActiveLink(localStorage.getItem('activeLink') || window.location.pathname);
+    };
+
+    window.addEventListener('popstate', handlePopState);
+
+    return () => window.removeEventListener('popstate', handlePopState);
+  }, []);
 
   return (
     <nav>
       <ul className="list-disc flex space-x-4 list-inside">
         <li>
-          <Link className={getClassNames('/posts/create')} href="/posts/create">Create</Link>
+          <Link
+            className={getClassNames('/posts/create')}
+            href="/posts/create"
+            onClick={() => handleClick('/posts/create')}
+          >
+            Create
+          </Link>
         </li>
         <li>
-          <Link className={getClassNames('/posts')} href="/posts">Posts</Link>
+          <Link className={getClassNames('/posts')} href="/posts" onClick={() => handleClick('/posts')}>
+            Posts
+          </Link>
         </li>
         <li>
-          <Link className={getClassNames('/logout')} href="/logout" method="post" data={{ foo: 'bar' }}>Logout</Link>
+          <Link
+            className={getClassNames('/logout')}
+            href="/logout"
+            method="post"
+            data={{ foo: 'bar' }}
+            onClick={() => handleClick('/logout')}
+          >
+            Logout
+          </Link>
         </li>
       </ul>
     </nav>
