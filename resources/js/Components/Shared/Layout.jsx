cf8d8c5c0a8539adc7f5d9033bb623df87d4017b
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-09 15:46:39 +0330

    feat (layout) : add the text in the welcome massage

--- resources/js/Components/Shared/Layout.jsx
+++ resources/js/Components/Shared/Layout.jsx
@@ -11,7 +11,7 @@ const user = usePage().props.auth.user;
                 <header className="flex justify-between">
                     <div className="flex items-center">
                         <h1 className="font-bold text-lg">My App</h1>
-                        <p className="text-sm ml-4">Welcome back, { user.username }</p>
+                        <p className="text-sm ml-4">Welcome back, { user.username }!</p>
                     </div>
                     <Navigation />
                 </header>
