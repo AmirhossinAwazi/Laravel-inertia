07fa4e5e49f883d66d1de63a02d0a19928cf8eb5
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-09 14:51:08 +0330

    feat (layout) : give the props in the layout

--- resources/js/Components/Shared/Layout.jsx
+++ resources/js/Components/Shared/Layout.jsx
@@ -1,14 +1,17 @@
+import { usePage } from "@inertiajs/inertia-react"
 import Navigation from "./Navigation"
 
 
 export const Layout = ({ children }) => {
+const user = usePage().props.auth.user;
+
     return (
         <div>
             <section className="p-6 bg-gray-200">
                 <header className="flex justify-between">
                     <div className="flex items-center">
                         <h1 className="font-bold text-lg">My App</h1>
-                        <p className="text-sm ml-4">Welcome back</p>
+                        <p className="text-sm ml-4">Welcome back, { user.username }</p>
                     </div>
                     <Navigation />
                 </header>
