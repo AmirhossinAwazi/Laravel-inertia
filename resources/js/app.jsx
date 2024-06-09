c2cac69f6c74610f0f2bb0995bb5d2276407d7df
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-09 15:50:25 +0330

    refactor (app.jsx)

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import {createRoot} from 'react-dom/client'
-import {createInertiaApp } from '@inertiajs/inertia-react'
-import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers'
+import { createRoot } from 'react-dom/client'
+import { createInertiaApp } from '@inertiajs/inertia-react'
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers'
 import { InertiaProgress } from '@inertiajs/progress'
 
 createInertiaApp({
-    // Below you can see that we are going to get all React components from resources/js/Pages folder
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`,import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
     setup({ el, App, props }) {
         createRoot(el).render(<App {...props} />)
     },
@@ -14,16 +13,9 @@ createInertiaApp({
 
 InertiaProgress.init(
     {
-        // The delay after which the progress bar will appear, in milliseconds...
         delay: 250,
-    
-        // The color of the progress bar...
         color: 'red',
-    
-        // Whether to include the default NProgress styles...
         includeCSS: true,
-    
-        // Whether the NProgress spinner will be shown...
         showSpinner: true,
-      },
-    )
+    },
+)
