89d993965baa36ebd687eb13eb92209672fac946
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-10 14:51:17 +0330

    feat (pages) : add the Settings page

---
+++ resources/js/Pages/Settings.jsx
@@ -0,0 +1,14 @@
+import Layout from '@/Components/Shared/Layout';
+
+const Settings = () => {
+
+    return (
+        <>
+            <h1 className="mt-6 text-3xl">Settings</h1>
+        </>
+    );
+};
+
+Settings.layout = (page) => <Layout>{page}</Layout>;
+
+export default Settings;
