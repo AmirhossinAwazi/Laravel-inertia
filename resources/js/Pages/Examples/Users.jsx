92ddbdae7b0c0dbf9908131f7768455e323fb088
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-10 19:25:16 +0330

    feat (page) : get the name of users and add in to the table

--- resources/js/Pages/Examples/Users.jsx
+++ resources/js/Pages/Examples/Users.jsx
@@ -6,14 +6,36 @@ const Users = ({ Users }) => {
         <>
             <h1 className="text-3xl">Users</h1>
 
-            <ul>
-                {Users.map(user => (
-                    <li key={user.id}>
-                        {user.name}
-                    </li>
-                ))}
-            </ul>
+            <div className="flex flex-col">
+                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table className="min-w-full divide-y- divide-gray-200">
+                                <tbody className="bg-white divide-y- divide-gray-200">
+                                    {Users.map(user => (
+                                        <tr>
+                                            <td className="px-6 py-4 whitespace-nowrap">
+                                                <div className="flex items-center">
+                                                    <div>
+                                                        <div className="text-sm font-medium text-gray-900">
+                                                            {user.name}
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </td>
 
+                                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                                <a href="#" className="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                            </td>
+                                        </tr>
+                                    ))}
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
