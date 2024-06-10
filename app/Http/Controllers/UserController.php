ebef0580dcf4112fba3a6fd596cacfca7ccb1376
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-10 21:07:23 +0330

    feat (Controller) : give the parametr to Users page and add the pagination

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::paginate(10);
+
+        return Inertia::render('Examples/Users', [
+            'Users' => UserResource::collection($users),
+            'pagination' => [
+                'current_page' => $users->currentPage(),
+                'last_page' => $users->lastPage(),
+                'total' => $users->total(),
+                'per_page' => $users->perPage(),
+            ],
+        ]);
+
+    }
 }
