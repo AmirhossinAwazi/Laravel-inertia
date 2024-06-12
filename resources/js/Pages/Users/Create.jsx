8b959f8a765583e412d58307a948458ca8acfe69
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-12 20:07:38 +0330

    feat (page) : add the create page for users

--- resources/js/Pages/Users/Create.jsx
+++ resources/js/Pages/Users/Create.jsx
@@ -1,15 +1,55 @@
+import React from 'react';
+import { useForm } from '@inertiajs/inertia-react';
 import Layout from '@/Components/Shared/Layout';
 
-const Create = ({}) => {
+const Create = () => {
+    const { data, setData, post, processing, errors } = useForm({
+        name: '',
+        email: '',
+        password: '',
+    });
+
+    function handleChange(e) {
+        const key = e.target.id;
+        const value = e.target.value;
+        setData(key, value);
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        post('/users');
+    }
+
     return (
         <>
-            <h1 className="text-3xl">Create New User</h1>
+            <h1 className="text-3xl font-bold">Create New User</h1>
 
-            <form action=""></form>
+            <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-8">
+                <div>
+                    <label htmlFor="name" className="block mb-2 uppercase font-bold text-sm text-gray-700">Name:</label>
+                    <input id="name" value={data.name} onChange={handleChange} required className="border border-gray-400 p-2 w-full" />
+                    {errors.name && <div className="text-red-600">{errors.name}</div>}
+                </div>
 
+                <div>
+                    <label htmlFor="email" className="block mb-2 uppercase font-bold text-sm text-gray-700">Email:</label>
+                    <input id="email" type="email" value={data.email} onChange={handleChange} required className="border border-gray-400 p-2 w-full" />
+                    {errors.email && <div className="text-red-600">{errors.email}</div>}
+                </div>
+
+                <div>
+                    <label htmlFor="password" className="block mb-2 uppercase font-bold text-sm text-gray-700">Password:</label>
+                    <input id="password" type="password" value={data.password} onChange={handleChange} required className="border border-gray-400 p-2 w-full" />
+                    {errors.password && <div className="text-red-600">{errors.password}</div>}
+                </div>
+                
+                <div className="mb-6 mt-4">
+                    <button type="submit" disabled={processing} className="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">Submit</button>
+                </div>
+            </form>
         </>
-    )
-};
+    );
+}
 
 Create.layout = (page) => <Layout>{page}</Layout>;
 
