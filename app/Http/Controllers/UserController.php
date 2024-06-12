eee2a85267fb0edb5cf317b9cf20503988f71f27
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-12 20:17:01 +0330

    feat (UserController) : add the method store for create user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 
 class UserController extends Controller
@@ -30,5 +32,22 @@ class UserController extends Controller
             ],
             'searchQuery' => $search,
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        User::created([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return Redirect::route('users.index')->with('success', 'User created successfully.');
     }
 }
