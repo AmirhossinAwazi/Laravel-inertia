ba66bb4a59b3d3e8d83c106c02dfb3efb7262ad7
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-14 21:08:20 +0330

    feat (usercontroller) : add the other method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,26 +11,31 @@ use Inertia\Inertia;
 
 class UserController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $search = $request->input('search');
+        return Inertia::render('Users/Index');
+    }
 
-        $users = User::query()
-            ->when($search, function ($query, $search) {
-                $query->where('name', 'like', "%{$search}%");
-            })
-            ->paginate(10);
+    public function create()
+    {
+        return Inertia::render('Users/Create');
+    }
 
-        return Inertia::render('Users/Index', [
-            'Users' => UserResource::collection($users),
-            'pagination' => [
-                'current_page' => $users->currentPage(),
-                'last_page' => $users->lastPage(),
-                'total' => $users->total(),
-                'per_page' => $users->perPage(),
-            ],
-            'searchQuery' => $search,
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
         ]);
+
+        User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return Redirect::route('users.index')->with('success', 'User created successfully.');
     }
 
     public function show(Request $request)
@@ -55,25 +60,18 @@ class UserController extends Controller
         ]);
     }
 
-    public function create()
+    public function edit()
     {
-        return Inertia::render('Users/Create');
+        //
     }
 
-    public function store(Request $request)
+    public function update()
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
-        ]);
+        //
+    }
 
-        User::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => Hash::make($request->input('password')),
-        ]);
-
-        return Redirect::route('users.index')->with('success', 'User created successfully.');
+    public function destroy()
+    {
+        //
     }
 }
