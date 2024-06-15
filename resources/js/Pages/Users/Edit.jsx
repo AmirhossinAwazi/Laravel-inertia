9eadc4c8c5724c2e1408fc46868d102ca5d15d32
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-06-15 15:04:22 +0330

    feat (page) : add the page edit user

---
+++ resources/js/Pages/Users/Edit.jsx
@@ -0,0 +1,19 @@
+
+import Layout from '@/Components/Shared/Layout';
+
+
+
+const Edit = () => {
+
+
+    return (
+        <>
+            <h1>hoooooooooooooy</h1>
+        </>
+    )
+
+}
+
+Edit.layout = (page) => <Layout>{page}</Layout>;
+
+export default Edit;
